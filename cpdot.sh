0c3ae1efaed3b555b5fa21eaa46224352ce3f02b
drevyek <rkettleson@gmail.com>
2016-05-08 18:03:48 -0300

    adding autoinstall script

--- cpdot.sh
+++ cpdot.sh
@@ -1,6 +1,11 @@
 # /bin/bash 
 
 # Script to copy these dot profiles into the home directory, and update them
+if [ -z $1 ]; then
+  echo "copying dotfiles"
+  find . -type f -name ".*" -print | xargs -i cp {} ~
+else
+  echo "getting dotfiles"
+  find . -maxdepth 1 -type f -name ".*" -print | xargs -i cp ~/{} .
+fi
 
-
-find . -type f -name ".*" -print | xargs -i cp {} ~
---
+++ getdeps.sh
@@ -0,0 +1,24 @@
+# I use ~/usrbin for my own personal installs; includied in the path by the .bashrc
+if [ ! -d ~/usrbin ]; then
+  echo "making ~/usrbin"
+  mkdir ~/usrbin 
+fi
+
+# To move all the dotprofiles to the ~ directory
+chmod +x ./cpdot.sh 
+./cpdot.sh
+
+# My vim requires ack as a dependency; (uses ack.vim)
+if [ ! -f ~/usrbin/ack ]; then
+  echo "Installing ack"
+  curl http://beyondgrep.com/ack-2.14-single-file > ~/usrbin/ack && chmod 0755 !#:3
+fi
+
+# To facilitate a fresh vim install
+
+if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
+  echo "Installing Vundle"
+  git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+  vim +PluginInstall +qall
+fi
+
