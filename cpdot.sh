6c893642c008970f80614b76e4f91c0c12574a5d
drevyek <rkettleson@gmail.com>
2016-05-09 04:40:48 -0300

    adding tmux, adding vifm, small edits elsewhere

--- cpdot.sh
+++ cpdot.sh
@@ -4,9 +4,15 @@
 if [ -z $1 ]; then
   echo "Copying dotfiles"
   find . -type f -name ".*" -print | xargs -i cp {} ~
+  if [ ! -d ~/.vifm ]; then
+    mkdir ~/.vifmrc
+    cp -r /usr/share/vifm/ ~/.vifm/
+  fi
+  cp vifmrc ~/.vifm/
 elif [ $1 = "-g" ]; then
   echo "Getting dotfiles"
   find . -maxdepth 1 -type f -name ".*" -print | xargs -i cp ~/{} .
+  cp ~/.vifm/vifmrc .
 else
   echo "Unknown option: $1"
   echo "Usage: ./cpdot.sh [-g]"
